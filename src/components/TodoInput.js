13ce3795f1ee476870e3c5063ec9bdd71c377eef
Bobby Martin <robert.martin.619@gmail.com>
2018-08-12 19:15:48 -0700

    Add styles to input button and list

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
 const TodoInput = (props) => {
+  const todoInput = {
+    padding: '15px',
+    flex: '2',
+    borderRadius: '8px 0 0 8px',
+    border: '1px solid steelblue',
+    borderRight: 'none'
+  }
+  const todoButton = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    border: '1px solid steelblue',
+    borderRadius: '0 8px 8px 0',
+    background: 'steelblue',
+    color: '#fff',
+    fontSize: '20px',
+    flex: '0.5',
+    padding: '0'
+  }
+  const form = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'cetner',
+    maxWidth: '300px',
+    height: '45px',
+    margin: 'auto',
+    marginTop: '300px',
+    position: 'relative'
+  }
   return (
     <div>
-      <form onSubmit={props.onSubmitTodo}>
-        <input placeholder='Enter something todo' onChange={props.onTextChange}/>
-        <button type='submit'>+</button>
+      <form style={form} onSubmit={props.onSubmitTodo}>
+        <input placeholder='Enter something todo' style={todoInput} onChange={props.onTextChange}/>
+        <button style={todoButton} type='submit'>+</button>
       </form>
     </div>
   )
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,14 +4,23 @@ import TodoContainer from './Todo';
 import TodoInput from './TodoInput';
 
 const TodoList = () => {
+  const todoList = {
+    listStyle: 'none',
+    maxWidth: '300px',
+    margin: 'auto',
+  }
+  const todoListItem = {
+    borderBottom: '1px solid steelblue',
+    padding: '20px',
+  }
   return (
       <Subscribe to={[TodoContainer]}>
       {todoListArr => (
         <div>
           <TodoInput onTextChange={todoListArr.handleTodoChange} onSubmitTodo={todoListArr.handleTodoSubmit}/>
-          <ul>
+          <ul style={todoListArr.state.todoList.length > 0 ? todoList : null}>
               {todoListArr.state.todoList.map((todo, index) => {
-                return <li key={index}>{todo}</li>
+                return <li style={todoListItem} key={index}>{todo}</li>
               })}
           </ul>
         </div>
